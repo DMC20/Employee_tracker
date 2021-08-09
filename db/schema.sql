94c21226b3a3a675ec720892c050db18e5dbd170
Daniel Carazo <danielcarazo20@gmail.com>
2021-08-09 14:47:39 -0400

    created sql files and js file in db folder

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY;
+    name VARCHAR(30) NOT NULL;
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY;
+    title VARCHAR(30) NOT NULL;
+    salary (10, 2) NOT NULL;
+    dept_id INTEGER;
+    FOREIGN KEY (department_id) REFERENCES department(id);
+);
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY;
+    first_name VARCHAR(30) NOT NULL;
+    last_name VARCHAR(30) NOT NULL;
+    role_id INTEGER;
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id);
+    FOREIGN KEY (manager_id) REFERENCES employees(id);
+);
---
+++ server.js
@@ -0,0 +1,5 @@
+const cTable = require('console.table');
+const inquirer = require('inquirer');
+const db = require('./db/connection');
+
+
